6694a25b53743f849c8db2e9843f02ca4a6ca245
HypoxanthineOvO <hyx021203@163.com>
2024-08-01 13:40:14 +0800

    Declare BSDF

---
+++ includes/bsdf.hpp
@@ -0,0 +1,16 @@
+#ifndef BSDF_HPP_
+#define BSDF_HPP_
+
+class BSDF {
+public:
+};
+
+class IdealDiffuseBSDF: public BSDF {
+public:
+};
+
+class IdealSpecularBSDF: public BSDF {
+public:
+};
+
+#endif // BSDF_HPP_
